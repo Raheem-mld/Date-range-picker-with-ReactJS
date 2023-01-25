07363cbc68961299e985de47bb60cab0d88abc04
Abderrahim Mouloudi <74796736+Raheem-mld@users.noreply.github.com>
2023-01-25 23:24:41 +0100

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -17,12 +17,15 @@ import List from '@mui/material/List';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
 import './App.css';
+import { addYears } from 'date-fns/esm';
 
 export default function App() {
 
   const [showCompare, setShowCompare] = useState(false)
 
   const [selectedIndex, setSelectedIndex] = React.useState(0);
+
+  const [label, setLabel] = useState("Today");
 
   const handleListItemClick = (event, index) => {
     setSelectedIndex(index);
@@ -46,7 +49,201 @@ export default function App() {
 
   const handleChange = (event) => {
     setPeriod(event.target.value);
+    switch (event.target.value) {
+      case 2:
+        setState({
+          ...state,
+          selection2: {
+            ...state.selection2,
+            startDate: addMonths(state.selection1.startDate, -1),
+            endDate: addMonths(state.selection1.endDate, -1),
+          }
+        })
+        break;
+      case 3:
+        setState({
+          ...state,
+          selection2: {
+            ...state.selection2,
+            startDate: addYears(state.selection1.startDate, -1),
+            endDate: addYears(state.selection1.endDate, -1),
+          }
+        })
+        break;
+
+      default:
+        break;
+    }
   };
+
+  const handlePresetDateClick = (index) => {
+    setPeriod(1);
+    switch (index) {
+      case 0:
+        setLabel("Today")
+        setState({
+          selection1: {
+            ...state.selection1,
+            startDate: new Date(),
+            endDate: new Date()
+          },
+          selection2: {
+            ...state.selection2,
+            startDate: addDays(new Date(), -1),
+            endDate: addDays(new Date(), -1),
+          }
+        })
+        break;
+
+      case 1:
+        setLabel("Yesterday")
+        setState({
+          selection1: {
+            ...state.selection1,
+            startDate: addDays(new Date(), -1),
+            endDate: addDays(new Date(), -1)
+          },
+          selection2: {
+            ...state.selection2,
+            startDate: addDays(new Date(), -2),
+            endDate: addDays(new Date(), -2),
+          }
+        })
+        break;
+
+      case 2:
+        setLabel("Last 7 days")
+        setState({
+          selection1: {
+            ...state.selection1,
+            startDate: addDays(new Date(), -7),
+            endDate: addDays(new Date(), -1)
+          },
+          selection2: {
+            ...state.selection2,
+            startDate: addDays(new Date(), -14),
+            endDate: addDays(new Date(), -8),
+          }
+        })
+        break;
+
+      case 3:
+        setLabel("Last 14 days")
+        setState({
+          selection1: {
+            ...state.selection1,
+            startDate: addDays(new Date(), -14),
+            endDate: addDays(new Date(), -1)
+          },
+          selection2: {
+            ...state.selection2,
+            startDate: addDays(new Date(), -28),
+            endDate: addDays(new Date(), -15)
+          },
+        })
+        break;
+
+      case 4:
+        setLabel("Last 30 days")
+        setState({
+          selection1: {
+            ...state.selection1,
+            startDate: addDays(new Date(), -30),
+            endDate: addDays(new Date(), -1)
+          },
+          selection2: {
+            ...state.selection2,
+            startDate: addDays(new Date(), -60),
+            endDate: addDays(new Date(), -31)
+          }
+        })
+        break;
+
+      case 5:
+        setLabel("Last week")
+        var curr = new Date; // get current date
+        var first = curr.getDate() - curr.getDay();
+        first = first - 7
+        var comparedFirst = first - 7;
+        var firstdayOb = new Date(curr.setDate(first));
+        var comparedfirstdayOb = new Date(curr.setDate(comparedFirst));
+        var firstday = firstdayOb.toDateString();
+        var firstdayTemp = firstdayOb;
+        var lastday = new Date(firstdayTemp.setDate(firstdayTemp.getDate() + 6));
+        setState({
+          selection1: {
+            ...state.selection1,
+            startDate: new Date(firstday),
+            endDate: lastday,
+          },
+          selection2: {
+            ...state.selection2,
+            startDate: new Date(comparedfirstdayOb.toDateString()),
+            endDate: addDays(new Date(firstday), -1),
+          }
+        })
+        break;
+
+      case 6:
+        setLabel("This week")
+        var curr = new Date; // get current date
+        var first = curr.getDate() - curr.getDay();
+        var firstdayOb = new Date(curr.setDate(first));
+
+        var curr2 = new Date;
+        var start = curr2.getDay();
+        setState({
+          selection1: {
+            ...state.selection1,
+            startDate: firstdayOb,
+            endDate: new Date()
+          },
+          selection2: {
+            ...state.selection2,
+            startDate: addDays(firstdayOb, - (start + 1)),
+            endDate: addDays(firstdayOb, -1)
+          }
+        })
+        break;
+
+      case 7:
+        setLabel("Last month")
+        var curr = new Date;
+        setState({
+          selection1: {
+            ...state.selection1,
+            startDate: addMonths(addDays(new Date(), -curr.getDate() + 1), -1),
+            endDate: addDays(new Date(), -curr.getDate())
+          },
+          selection2: {
+            ...state.selection2,
+            startDate: addMonths(addDays(new Date(), -curr.getDate() + 1), -2),
+            endDate: addMonths(addDays(new Date(), -curr.getDate()), -1),
+          },
+        })
+        break;
+
+      case 8:
+        setLabel("This month")
+        var curr = new Date;
+        setState({
+          selection1: {
+            ...state.selection1,
+            startDate: addDays(new Date(), -curr.getDate() + 1),
+            endDate: new Date()
+          },
+          selection2: {
+            ...state.selection2,
+            startDate: addDays(new Date(), -curr.getDate() - curr.getDate() + 1),
+            endDate: addDays(new Date(), -curr.getDate())
+          }
+        })
+        break;
+
+      default:
+        break;
+    }
+  }
 
   const handleStartDateChange = (value) => {
     if (value.$d != "Invalid Date") {
@@ -59,124 +256,6 @@ export default function App() {
           key: 'selection2'
         }
       })
-    }
-  }
-
-  const handlePresetDateClick = (index) => {
-    switch (index) {
-      case 0:
-        setState({
-          ...state,
-          selection1: {
-            ...state.selection1,
-            startDate: new Date(),
-            endDate: new Date()
-          }
-        })
-        break;
-
-      case 1:
-        setState({
-          ...state,
-          selection1: {
-            ...state.selection1,
-            startDate: addDays(new Date(), -1),
-            endDate: addDays(new Date(), -1)
-          }
-        })
-        break;
-
-      case 2:
-        setState({
-          ...state,
-          selection1: {
-            ...state.selection1,
-            startDate: addDays(new Date(), -7),
-            endDate: addDays(new Date(), -1)
-          }
-        })
-        break;
-
-      case 3:
-        setState({
-          ...state,
-          selection1: {
-            ...state.selection1,
-            startDate: addDays(new Date(), -14),
-            endDate: addDays(new Date(), -1)
-          }
-        })
-        break;
-
-      case 4:
-        setState({
-          ...state,
-          selection1: {
-            ...state.selection1,
-            startDate: addDays(new Date(), -30),
-            endDate: addDays(new Date(), -1)
-          }
-        })
-        break;
-
-      case 5:
-        var curr = new Date; // get current date
-        var first = curr.getDate() - curr.getDay();
-        first = first - 7
-        var firstdayOb = new Date(curr.setDate(first));
-        var firstday = firstdayOb.toDateString();
-        var firstdayTemp = firstdayOb;
-        var lastday = new Date(firstdayTemp.setDate(firstdayTemp.getDate() + 6));
-        setState({
-          ...state,
-          selection1: {
-            ...state.selection1,
-            startDate: new Date(firstday),
-            endDate: lastday,
-          }
-        })
-        break;
-
-      case 6:
-        var curr = new Date; // get current date
-        var first = curr.getDate() - curr.getDay();
-        var firstdayOb = new Date(curr.setDate(first));
-        setState({
-          ...state,
-          selection1: {
-            ...state.selection1,
-            startDate: firstdayOb,
-            endDate: new Date()
-          }
-        })
-        break;
-
-      case 7:
-        var curr = new Date;
-        setState({
-          ...state,
-          selection1: {
-            ...state.selection1,
-            startDate: addMonths(addDays(new Date(), -curr.getDate() + 1), -1),
-            endDate: addDays(new Date(), -curr.getDate())
-          }
-        })
-        break;
-
-      case 8:
-        var curr = new Date;
-        setState({
-          ...state,
-          selection1: {
-            ...state.selection1,
-            startDate: addDays(new Date(), -curr.getDate() + 1),
-            endDate: new Date()
-          }
-        })
-        break;
-
-      default:
-        break;
     }
   }
 
@@ -194,6 +273,31 @@ export default function App() {
     }
   }
 
+  const handleComparedStartDateChange = (value) => {
+    if (value.$d != "Invalid Date") {
+      setPeriod(4)
+      setState({
+        ...state,
+        selection2: { ...state.selection2, startDate: new Date(value) },
+      })
+    }
+  }
+
+
+  const handleComparedEndDateChange = (value) => {
+    if (value.$d != "Invalid Date") {
+      setPeriod(4)
+      setState({
+        ...state,
+        selection2: { ...state.selection2, endDate: new Date(value) },
+      })
+    }
+  }
+
+  const handleSwitch = (e) => {
+    setShowCompare(e.target.checked);
+  }
+
   return (
 
     <LocalizationProvider dateAdapter={AdapterDayjs}>
@@ -205,7 +309,7 @@ export default function App() {
               <div className="row-container">
                 <div className="thisMonthContainer">
                   <div className="square"></div>
-                  <div className="label">This week</div>
+                  <div className="label">{label}</div>
                 </div>
                 <div className="inputs">
                   <DesktopDatePicker
@@ -244,6 +348,7 @@ export default function App() {
                 <div className="inputs">
                   <DesktopDatePicker
                     value={state.selection2.startDate}
+                    onChange={(newValue) => { handleComparedStartDateChange(newValue) }}
                     renderInput={(params) => <TextField {...params} />}
                     inputFormat="MM/DD/YYYY"
                     size='small'
@@ -251,6 +356,7 @@ export default function App() {
                   -
                   <DesktopDatePicker
                     value={state.selection2.endDate}
+                    onChange={(newValue) => { handleComparedEndDateChange(newValue) }}
                     renderInput={(params) => <TextField {...params} />}
                     inputFormat="MM/DD/YYYY"
                     size='small'
@@ -260,7 +366,7 @@ export default function App() {
             </div>
             <div className="switch-container">
               <div className="label">Compare</div>
-              <Switch onChange={(e) => { setShowCompare(e.target.checked) }} />
+              <Switch onChange={(e) => { handleSwitch(e) }} />
             </div>
           </div>
 
@@ -286,6 +392,7 @@ export default function App() {
         </div>
         <div className="datePreset">
           <List component="nav" aria-label="main mailbox folders">
+          <ListItemText style={{ fontWeight: '900' }} primary="Date presets" />
             <ListItemButton
               selected={selectedIndex === 0}
               onClick={(event) => { handleListItemClick(event, 0); handlePresetDateClick(0) }}
